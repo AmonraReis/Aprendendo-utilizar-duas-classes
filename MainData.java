02ba6a07b21f4b17af0bc0c2db969a17b1202fd0
AmonraReis <75175885+AmonraReis@users.noreply.github.com>
2021-04-12 15:29:51 -0300

    Add files via upload
    Teste de aprendizado com duas classes.

---
+++ MainData.java
@@ -0,0 +1,16 @@
+
+public class MainData {
+	
+	public static void main(String[] args) {
+		Data d1 = new Data();
+		
+		d1.dia = 2;
+		d1.mes= 5;
+		d1.ano= 2021;
+		
+		System.out.printf("%d/%d/%d",d1.dia, d1.mes, d1.ano);
+		
+		
+	}
+
+}
